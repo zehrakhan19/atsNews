d17101643741e1cb1807cfd674a8eb3df5c384dd
zehrakhan19 <zehraamirkhan19@gmail.com>
2023-10-11 11:44:01 +0530

    Done Checking

--- src/app/screens/auth/login-with-mobile-view/LoginWithMobileView.tsx
+++ src/app/screens/auth/login-with-mobile-view/LoginWithMobileView.tsx
@@ -23,89 +23,81 @@ function LoginWithMobileView({
   isLoadingLogin,
 }) {
   return (
-    <>
-      <ScrollView style={{backgroundColor: '#fff'}}>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View>
-              <View style={{alignItems: 'flex-end', padding: 20}}>
+    <ScrollView style={{backgroundColor: '#fff'}}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View>
+            <View style={{alignItems: 'flex-end', padding: 20}}>
+              <Text
+                style={[
+                  iOSUIKit.subheadEmphasizedObject,
+                  systemWeights.bold,
+                  ,
+                  {color: iOSColors.gray},
+                ]}>
+                SKIP
+              </Text>
+            </View>
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Image
+                source={require('../../../assets/loginImage.jpg')}
+                style={{
+                  width: 450,
+                  height: 300,
+                  resizeMode: 'contain',
+                }}
+              />
+            </View>
+            <View style={{gap: 8, padding: 10}}>
+              <Text
+                style={[iOSUIKit.largeTitleEmphasized, systemWeights.regular]}>
+                Hey,
+              </Text>
+              <Text
+                style={[iOSUIKit.largeTitleEmphasized, systemWeights.regular]}>
+                Login Now
+              </Text>
+              <View style={{flexDirection: 'row', gap: 4}}>
+                <Text style={[iOSUIKit.subhead, systemWeights.regular]}>
+                  if you are new /
+                </Text>
                 <Text
                   style={[
                     iOSUIKit.subheadEmphasizedObject,
                     systemWeights.bold,
-                    ,
-                    {color: iOSColors.gray},
                   ]}>
-                  SKIP
+                  Create New
                 </Text>
               </View>
-              <View
-                style={{
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Image
-                  source={require('../../../assets/loginImage.jpg')}
-                  style={{
-                    width: 450,
-                    height: 300,
-                    resizeMode: 'contain',
-                  }}
+              <View style={{paddingVertical: 14}}>
+                <InputText
+                  placeholder="Enter Mobile Number"
+                  value={mobileNumber}
+                  onChangeText={text => checkMobileNumberValidity(text)}
+                  keyboardType="number-pad"
+                  maxLength={10}
                 />
               </View>
-              <View style={{gap: 8, padding: 10}}>
-                <Text
-                  style={[
-                    iOSUIKit.largeTitleEmphasized,
-                    systemWeights.regular,
-                  ]}>
-                  Hey,
-                </Text>
-                <Text
-                  style={[
-                    iOSUIKit.largeTitleEmphasized,
-                    systemWeights.regular,
-                  ]}>
-                  Login Now
-                </Text>
-                <View style={{flexDirection: 'row', gap: 4}}>
-                  <Text style={[iOSUIKit.subhead, systemWeights.regular]}>
-                    if you are new /
-                  </Text>
-                  <Text
-                    style={[
-                      iOSUIKit.subheadEmphasizedObject,
-                      systemWeights.bold,
-                    ]}>
-                    Create New
-                  </Text>
-                </View>
-                <View style={{paddingVertical: 14}}>
-                  <InputText
-                    placeholder="Enter Mobile Number"
-                    value={mobileNumber}
-                    onChangeText={text => checkMobileNumberValidity(text)}
-                    keyboardType="number-pad"
-                    maxLength={10}
-                  />
-                </View>
-              </View>
             </View>
-          </TouchableWithoutFeedback>
-          <View style={{padding: 10}}>
-            <Button
-              label="Login"
-              size="large"
-              callback={() => {
-                sendOTP();
-              }}
-              disabled={sendOtpDisable || isLoadingLogin}
-            />
           </View>
-        </KeyboardAvoidingView>
-      </ScrollView>
-    </>
+        </TouchableWithoutFeedback>
+        <View style={{padding: 10}}>
+          <Button
+            label="Login"
+            size="large"
+            callback={() => {
+              sendOTP();
+            }}
+            disabled={sendOtpDisable || isLoadingLogin}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 }
 
